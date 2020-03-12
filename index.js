251a49ad84ff23a4e67cffdca4f5cc92e39eed67
AidanLincoln <aidantlincoln@gmail.com>
2020-03-12 09:13:01 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Add your code here
+
+
+function submitData(name, email) {
+    const configObj = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name: name,
+            email: email
+        }) 
+    }
+    return fetch('http://localhost:3000/users', configObj)
+    .then(function(response) {
+        return response.json();
+    })   
+    .then(function(object) {
+        const textNode = document.createTextNode(object.id)
+        document.body.appendChild(textNode)
+    })
+    .catch(function(error) {
+        alert("Bad things! Ragnarők!");
+        const textNode = document.createTextNode(error.message)
+        document.body.appendChild(textNode)
+    });
+};
