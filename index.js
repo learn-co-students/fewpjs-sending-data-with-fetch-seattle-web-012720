b669d1aec3a3782740212a6c6af09d37f6fe44ad
kailanak1 <kailanakahawaii@gmail.com>
2020-03-12 16:34:20 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Add your code here
+
+//import { fileMatch } from "file-system";
+
+
+
+const userUrl = 'http://localhost:3000/users'
+
+function submitData(name, email){
+
+    let configObj = {
+        method: "POST",
+        headers: { 
+            "Content-Type": "application/json", 
+            "Accept": "application/json"
+        }, 
+        body: JSON.stringify({name, email})
+    };
+    return fetch(userUrl, configObj)
+    .then(function(res){
+        return res.json();
+    })
+    .then(function(json) {
+        document.body.innerHTML = json.id;
+    })
+    .catch(function(error){
+        alert("failed")
+        document.body.innerHTML = error.message;
+    });
+}
+
+submitData('steve', 'steve@steve.mail')
