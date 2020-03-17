b9cc46b8d93025d83daf890c1e90d66d2c9c84e0
unclezap <zeb.blackwell@gmail.com>
2020-03-17 04:47:29 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
+
+
 // Add your code here
+document.addEventListener("DOMContentLoaded", function() {
+    
+})
+
+function submitData(name, email) {
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name,
+            email
+        })
+    })
+    .then(function(response) {
+        return response.json()
+    })
+    .then(function(data) {
+        console.log(data)
+        document.body.innerHTML = data["id"]
+    })
+    .catch(function(error) {
+        alert("ALERT")
+        document.body.innerHTML = error.message
+    })
+}
